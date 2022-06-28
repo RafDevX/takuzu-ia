161f5245a6bf67da2b37e9a27dad38b1aa9937cd
Rafael Oliveira <rafdev.x@gmail.com>
2022-06-28 18:27:55 +0100

    allow custom tests

--- stats.sh
+++ stats.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
+tests_dir="./tests/input_*"
+if [ "$1" = "custom-tests" ]; then
+	tests_dir="../proj-ist-unit-tests/ia/2021-2022/custom-tests/*.in"
+fi
+
 echo "test,search,mean_time,gend_nodes,expd_nodes"
 
-for i in $(ls tests/input_*); do
+for i in $(ls $tests_dir); do
 	for j in "bfs" "dfs" "greedy" "a_star"; do
 		nodes_stats=$(python takuzu.py $j < $i | tail -n1)
 		gen_nodes=$(echo $nodes_stats | cut -d' ' -f1)
